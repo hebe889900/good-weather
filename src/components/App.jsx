de8bb2be778ed692618568fd00e352186b9185ad
jackyan63@gmail.com <jackyan63@gmail.com>
2018-02-11 02:07:05 -0500

    Reading through the react document

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,161 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import he from 'he';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import AppBar from 'material-ui/AppBar';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-import FlatButton from 'material-ui/FlatButton';
-import Divider from 'material-ui/Divider';
-import axios from 'axios';
 
 
 
-const MyNavLinks = () => (
-  <ToolbarGroup>
-    <FlatButton label="Dashboard" labelStyle = {{color: "white"}}/>
-    <FlatButton label="Settings" labelStyle = {{color: "white"}}/>
-    <FlatButton label="Profile" labelStyle = {{color: "white"}}/>
-  </ToolbarGroup> 
-);
+class ProductCategoryRow extends React.Component {
+  render() {
+    const name = this.props.name;
+    return (
+      <tr>
+        <th colSpan="2">
+          {name}
+        </th>
+      </tr>
+    );
+  }
+}
 
-class App extends React.Component {
-   render() {
-      return (
-      	<MuiThemeProvider>
-      		 <div style = {this.props.styles}>
-		         <div className="container">
-		         	<AppBar style = {this.props.appBarStyle} title="Forecast" iconElementRight={<MyNavLinks />} />
-			         	<Card>
-			         	<CardHeader titleStyle = {{fontSize: "2em",fontWeight: "bold"}} title= {this.props.title} titleColor = {this.props.titleColor} actAsExpander={true} showExpandableButton={true}/>	
-			         	<Divider />
-			            <CardText style = {{fontSize: "1.5em"}} color = {this.props.color}>{this.props.content}</CardText>		            
-			            <CardActions>
-			            	<div id = "button"></div>
-			            	<div id = "socialbutton"></div>
-			            </CardActions>
-			            </Card>
-		         </div>
-	         </div>
-         </MuiThemeProvider>
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+
+
+
+    return (
+      <tr>
+        <td>{product.main}</td>
+        <td>{product.icon}</td>
+      </tr>
+    );
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
+    const rows = [];
+
+
+      console.log(this.props.products)
+
+
+      rows.push(
+        <ProductRow
+          product={this.props.products.data.name}
+          key={this.props.products.data.name}
+        />
       );
-   }
+
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
 }
 
-handleClick();
-
-function handleClick(e){
-	if ("geolocation" in navigator) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-		console.log(position.coords.latitude, position.coords.longitude);
-		let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=4b8d06411db9758c752cb3889b3a220e&lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
-		axios.get(apiUrl).then(function (response) {
-			//alert("clicked");
-			console.log(response);	
-			ReactDOM.render(
-			<App styles = {{fontFamily: "'Futura URW',sans-serif"}} title={response.data.main.temp} content = {response.data.weather[0].main}/>,
-			document.getElementById('app')
-			);		
-	
-		})
-		.catch(function (error) {
-			console.log(error);
-		});  
-	});
-	} else {
-	  /* geolocation IS NOT available */
-	}	
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+  
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+  
+  handleInStockChange(e) {
+    this.props.onInStockChange(e.target.checked);
+  }
+  
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
+        />
+        <p>
+          <input
+            type="checkbox"
+            checked={this.props.inStockOnly}
+            onChange={this.handleInStockChange}
+          />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
 }
 
-export default App;
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+    
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+  
+  handleInStockChange(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default FilterableProductTable;
