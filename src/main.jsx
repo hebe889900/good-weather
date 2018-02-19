f40d29e921dfc302d09c2bf4258e49c5fc22f2d0
jackyan63@gmail.com <jackyan63@gmail.com>
2018-02-19 04:40:06 -0500

    The app can finally show the temperature dynamically

--- src/main.jsx
+++ src/main.jsx
@@ -4,209 +4,89 @@ import axios from 'axios';
 
 
 
-
-
-
-// CurrentWeather passes data to the weather header and the weather summary
-class CurrentWeather extends React.Component {
-    render() {
-        return (
-            <div className="z-depth-3 white">
-                <WeatherHeader weather={this.props.weather} units={this.props.units} onClick={this.props.onClick}/>
-                <WeatherSummary weather={this.props.weather}/>
-            </div>
-        )
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      loading: true,
+      zipcode: 10003,
+      temp: 20,
+      name: 'New York',
+      humidity: 50,
+      sunrise: 7,
+      value: ''
     }
-}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  componentDidMount() {
+  	let th = this;
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+		console.log(position.coords.latitude, position.coords.longitude);
+		let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=4b8d06411db9758c752cb3889b3a220e&lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
+		axios.get(apiUrl).then(function (response) {
+			//alert("clicked");
+			console.log(th);
+			console.log(response);		
+			th.setState({loading: false, name: response.data.name, temp: Math.round(parseInt((response.data.main.temp)- 273)*9/5 +32), humidity: response.data.main.humidity})
+			
+		})
+		.catch(function (error) {
+			console.log(error);
+		});  
+	});
+	} else {
+	  /* geolocation IS NOT available */
+	}
 
-// Main header for current weather.
-function WeatherHeader(props) {
-    let icon = "wi wi-large wi-forecast-io-" + props.weather.icon
-    return (
-        <div className="white center-align">
-            <h1>{Math.ceil(props.weather.temperature)}° {props.units}</h1>
-            <i className={icon}></i>
-            <p>{props.weather.summary}</p>
-            <div className="switch">
-                <label>
-                    F
-                    <input type="checkbox" onClick={props.onClick}/>
-                    <span className="lever"></span>
-                    C
-                </label>
-            </div>
-        </div>
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({zipcode: this.state.value});
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${this.state.zipcode}&appid=4398ca985b90b09bb540560e9dd6b60e`)
+    .then(res => res.json())
+    .then(res =>   
+    this.setState({
+      name: res.name,
+      temp: Math.round(parseInt((res.main.temp)- 273)*9/5 +32),
+      humidity: res.main.humidity
+    })
     )
-}
-
-// Shows the details about the current weather.
-function WeatherSummary(props) {
+    if (this.state.temp > 80){
+      document.getElementById('temp').style.color = 'red'
+    }
+    else if (this.state.temp < 50){
+      document.getElementById('temp').style.color = 'blue'
+    }
+  }
+  render(){
+    if (this.state.loading) {
+      return(<div>Loading...</div>);
+    } else {
     return (
-        <div>
-            <table className="striped">
-                <tbody>
-                    <tr>
-                        <td>Humidity: {Math.ceil(props.weather.humidity * 100)}%</td>
-                        <td>Cloud cover: {Math.ceil(props.weather.cloudCover * 100)}%</td>
-                    </tr>
-                    <tr>
-                        <td>Chance of precipitation: {props.weather.precipProbability * 100}%</td>
-                        <td>Closest storm distance: {props.weather.nearestStormDistance}
-                            miles</td>
-                    </tr>
-                    <tr>
-                        <td>Wind Speed: {Math.ceil(props.weather.windSpeed)}
-                            mph</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <div>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          <h1>Enter zip and press enter</h1>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+      </form>
+        <h1>City: {this.state.name}</h1>
+        <h2 id= 'temp'>Temp: {this.state.temp} &#x2109;</h2>
+        <h2>Humidity: {this.state.humidity}%</h2>
+     </div>
     )
+    }
+  }
 }
 
-// Graph of the weather over the next 24 hours
-// Hourly > HourData
-class Hourly extends React.Component {
-    render() {
-        let result = this.props.weather.data.map(function(hour) {
-            return hour.temperature
-        })
-        let hourlyData = this.props.weather.data.map(function(hour) {
-            return <HourData weather={hour}/>
-        })
-        hourlyData = hourlyData.slice(1, 7)
-        return (
-            <div className="z-depth-3 white">
-                <h3>Next 24 hours</h3>
-                <Trend smooth autoDraw autoDrawDuration={3000} autoDrawEasing="ease-out" data={result} gradient={['#42b3f4']} radius={7.7} strokeWidth={5} strokeLinecap={'butt'}/>
 
-                <div className="row">
-                    {hourlyData}
-                </div>
-            </div>
-        )
-    }
-}
 
-class HourData extends React.Component {
-    render() {
-        let d = moment.unix(this.props.weather.time).fromNow(true)
-        let icon = "wi wi-small wi-forecast-io-" + this.props.weather.icon
-        return (
-            <div className="col m2 s4">
-                <p>
-                    <b>In {d}:</b>
-                </p>
-                <i className={icon}></i>
-                <p>{Math.ceil(this.props.weather.temperature)}°</p>
-            </div>
-        )
-    }
-}
-
-// Graph of the data over the next week.
-// Daily > DayData
-class Daily extends React.Component {
-    render() {
-        let temperatures = new Array
-        let result = this.props.weather.data.map(function(day) {
-            temperatures.push(day.temperatureMin)
-            temperatures.push(day.temperatureMax)
-        })
-        let dailyData = this.props.weather.data.map(function(day) {
-            return <DayData weather={day}/>
-        })
-        dailyData = dailyData.slice(1, 8)
-        return (
-            <div className="z-depth-3 white">
-                <h3>Next 7 Days</h3>
-                <Trend smooth autoDraw autoDrawDuration={3000} autoDrawEasing="ease-out" data={temperatures} gradient={['#42b3f4']} radius={7.7} strokeWidth={5} strokeLinecap={'butt'}/> {dailyData}
-            </div>
-        )
-    }
-}
-
-class DayData extends React.Component {
-    render() {
-        let d = moment.unix(this.props.weather.time).format('dddd')
-        return (
-            <div>
-                <p>
-                    <b>{d}:</b>
-                    {this.props.weather.summary}</p>
-            </div>
-        )
-    }
-}
-
-//App > CurrentWeather > Head > WeatherData
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            weather: {},
-            data: false,
-            units: "F"
-        }
-        this.switchUnits = this.switchUnits.bind(this)
-    }
-
-    componentDidMount() {
-        this.getLocationAndWeather()
-    }
-
-    switchUnits() {
-        if (this.state.units == "F") {
-            this.setState({units: "C"})
-        } else {
-            this.setState({units: "F"})
-        }
-        this.getLocationAndWeather()
-    }
-
-    getLocationAndWeather() {
-        let th = this
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(function(pos) {
-                let lat = pos.coords.latitude
-                let lon = pos.coords.longitude
-                let units = (th.state.units == "F"
-                    ? "us"
-                    : "si")
-                let loc = lat + "," + lon + "?units=" + units
-                let url = "https://api.darksky.net/forecast/04c3762b193aaee40affb899992cef3e/" + loc
-                axios.get(url).then(function(response) {
-                    return response.json()
-                }).then(function(json) {
-                    th.setState({data: true, weather: json})
-                })
-            })
-        }
-    }
-
-    render() {
-        if (this.state.data) {
-            return (
-                <div className="row">
-                    <div className="col s12 m6 offset-m3">
-                        <CurrentWeather weather={this.state.weather.currently} onClick={this.switchUnits} units={this.state.units}/>
-                        <Hourly weather={this.state.weather.hourly} units={this.state.units}/>
-                        <Daily weather={this.state.weather.daily}/>
-                      <p>Powered by <a href="https://darksky.net/poweredby/">DarkSky</a></p>
-                    </div>
-                
-                </div>
-            )
-        } else {
-            return <div></div>;
-        }
-    }
-};
-
-ReactDOM.render(
-  <App/>, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById('root'));
 
 function initialize(){
 	if ("geolocation" in navigator) {
@@ -215,9 +95,7 @@ function initialize(){
 		let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=4b8d06411db9758c752cb3889b3a220e&lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
 		axios.get(apiUrl).then(function (response) {
 			//alert("clicked");
-			console.log(response);	
-
-				
+			console.log(response);					
 		})
 		.catch(function (error) {
 			console.log(error);
